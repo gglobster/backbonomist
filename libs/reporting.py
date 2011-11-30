cb01d1b7b99c814f41bbef6ed32a00317da2390b
Geraldine Van der Auwera <gege@mac.com>
2011-11-29 23:06:47 -0500

    switched plots format to pdf -- incomplete

--- libs/reporting.py
+++ libs/reporting.py
@@ -2,13 +2,8 @@ from os import path
 from config import fixed_dirs, run_dirs, p_root_dir, project_id, \
     project_date, genomes, references, ctg_thresholds
 from common import ensure_dir
+from plotting import plot_ctg_stats, hits_heatmap_multi
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
-from mpl_toolkits.axes_grid1 import make_axes_locatable, Size, AxesGrid
-
 
 def log_start_run(run_id, timestamp):
     """Record run initiated in the dataset log."""
@@ -116,35 +111,6 @@ def ctg_stats(g_name, records):
     ctg_cats = small_ctgs, mid_ctgs, big_ctgs
     plot_ctg_stats(ctg_cats, fname)
 
-def plot_ctg_stats(ctg_cats, fname):
-    """Plot distribution statistics of contigs per genome."""
-    small_ctgs, mid_ctgs, big_ctgs = ctg_cats
-    kb1, kb2, kb3 = ctg_thresholds
-    # make bins
-    factor = 10
-    small_bins = np.arange(0, kb1, kb1/factor)
-    mid_bins = np.arange(kb1, kb2, (kb2-kb1)/factor)
-    big_bins = np.arange(kb2, kb3, (kb3-kb2)/factor)
-    # plot histograms
-    fig = plt.figure(figsize=(16,6))
-    if not len(small_ctgs) == 0:
-        small_x = fig.add_subplot(131)
-        small_x.hist(small_ctgs, small_bins, rwidth=kb1/factor)
-        small_x.set_title("length < "+str(kb1)+" kb")
-        small_x.set_ylabel("number of contigs")
-    if not len(mid_ctgs) == 0:
-        mid_x = fig.add_subplot(132)
-        mid_x.hist(mid_ctgs, mid_bins, rwidth=(kb2-kb1)/factor)
-        mid_x.set_title(str(kb1)+" kb < length < "+str(kb2)+" kb")
-        mid_x.set_ylabel("number of contigs")
-    if not len(big_ctgs) == 0:
-        big_x = fig.add_subplot(133)
-        big_x.hist(big_ctgs, big_bins, rwidth=(kb3-kb2)/factor)
-        big_x.set_title("length > "+str(kb2)+" kb")
-        big_x.set_ylabel("number of contigs")
-    plt.savefig(fname)
-    plt.clf()
-
 def matches_table(run_ref, run_id, ref_hits, ctl_scores):
     """Compile tables and graphs of contig matches."""
     # set imputs and outputs
@@ -152,8 +118,8 @@ def matches_table(run_ref, run_id, ref_hits, ctl_scores):
     run_root = p_root_dir+run_id+"/"
     report_root = run_root+run_dirs['reports']+ref_n+"/"
     hits_table_txt = report_root+run_id+"_"+ref_n+"_hits_table.txt"
-    red_hits_fig = report_root+run_id+"_"+ref_n+"_sum_hits.png"
-    mf_hits_fig = report_root+run_id+"_"+ref_n+"_all_hits.png"
+    red_hits_fig = report_root+run_id+"_"+ref_n+"_sum_hits.pdf"
+    mf_hits_fig = report_root+run_id+"_"+ref_n+"_all_hits.pdf"
     ensure_dir([report_root])
     rep_fhandle = open(hits_table_txt, 'w')
     rep_fhandle.write("# Matches to the reference segments of "+ref_n)
@@ -166,7 +132,7 @@ def matches_table(run_ref, run_id, ref_hits, ctl_scores):
     for g_name in sorted(ref_hits, reverse=True):
         genome_root = report_root+g_name+"/"
         ensure_dir([genome_root])
-        g_table_fig = genome_root+g_name+"_hits_"+ref_n+".png"
+        g_table_fig = genome_root+g_name+"_hits_"+ref_n+".pdf"
         # collect scores
         g_list = []
         g_header = ["\n\n"+g_name] + segs
@@ -215,70 +181,3 @@ def matches_table(run_ref, run_id, ref_hits, ctl_scores):
     # done
     rep_fhandle.close()
 
-def hits_heatmap_multi(ref_n, segs, g_names, contigs, scores, imgfile):
-    """Combine matches heatmaps in subplots."""
-    g_names.reverse()
-    contigs.reverse()
-    scores.reverse()
-    ctg_count = 0
-    for item in contigs:
-        ctg_count += len(item)
-    fig = plt.figure(figsize=(len(segs)/2+1, ctg_count/2+3))
-    grid = AxesGrid(fig, 111, nrows_ncols=(len(g_names), 1), axes_pad=0.4,
-                    cbar_location="top", cbar_mode="single", cbar_size=0.1)
-    for i in range(len(g_names)):
-        hmap = grid[i].pcolor(scores[i], cmap='hot', vmin=0, vmax=1)
-        grid[i].xaxis.set_major_locator(MaxNLocator(len(segs)))
-        grid[i].yaxis.set_major_locator(MaxNLocator(len(scores[i])))
-        grid.cbar_axes[i].colorbar(hmap)
-        grid.cbar_axes[i].set_xticks([0, 0.5, 1])
-        grid.cbar_axes[i].set_xticklabels(['Low', 'Medium', 'High'])
-        grid[i].set_xticklabels(segs, size='small')
-        grid[i].set_xlabel(ref_n+" reference segments", size='small')
-        grid[i].set_title(g_names[i], size='small')
-        grid[i].set_yticklabels("")
-        labels = grid[i].get_xticklabels()
-        for label in labels:
-            label.set_rotation(30)
-        y_index = 0.5
-        for contig in contigs[i]:
-            grid[i].text(-0.2, y_index, contig, size='small',
-                         horizontalalignment='right',
-                         verticalalignment='center',)
-            y_index +=1
-    plt.savefig(imgfile)
-    plt.clf()
-
-def hits_heatmap(title, segs, contigs, scores, imgfile):
-    """Graph matches as a heatmap.
-
-    Deprecated in favor of the multi-plot version.
-
-    """
-    fig = plt.figure()
-    h = [Size.Fixed(1.5), Size.Fixed(15)]
-    v = [Size.Fixed(2.5), Size.Fixed(5.)]
-    ax = plt.subplot(111)
-    hmap = ax.pcolor(scores, cmap='hot', vmin=0, vmax=1)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("bottom", size="5%", pad=1)
-    cbar = plt.colorbar(hmap, cax=cax, ticks=[0, 0.5, 1],
-                    orientation='horizontal')
-    cbar.ax.set_xticklabels(['Low', 'Medium', 'High'])
-    ax.xaxis.set_major_locator(MaxNLocator(len(segs)))
-    ax.yaxis.set_major_locator(MaxNLocator(len(contigs)))
-    ax.set_xticklabels(segs, size='small')
-    ax.set_yticklabels("")
-    labels = ax.get_xticklabels()
-    for label in labels:
-        label.set_rotation(30)
-    y_index = 0.5
-    for contig in contigs:
-        ax.text(-0.2, y_index, contig, size='small',
-                horizontalalignment='right',
-                verticalalignment='center',)
-        y_index +=1
-    ax.set_xlabel("Reference segments", size='small')
-    ax.set_title(title)
-    plt.savefig(imgfile)
-    plt.clf()
